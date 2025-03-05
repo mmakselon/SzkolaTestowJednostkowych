dc66e44be8e65f253f3145450adf6b10e1470108
mmakselon <magdalena.pachacz@gmail.com>
2025-03-05 22:17:37 +0100

    add new project

---
+++ SzkolaTestowJednostkowych_UnitTests/Basics/CalculatorTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzkolaTestowJednostkowych_UnitTests.Basics
+{
+    class CalculatorTests
+    {
+        //NazwaMetody_Scenariusz_OczekiwanyRezultat
+        public void Add_WhenCalled_ShouldReturnSum()
+        {
+
+        }
+    }
+}
