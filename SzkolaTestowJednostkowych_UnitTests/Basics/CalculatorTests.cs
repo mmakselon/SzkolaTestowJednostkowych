c78275fbabcc67e3e09f94b64519797ada701483
mmakselon <magdalena.pachacz@gmail.com>
2025-03-06 11:48:03 +0100

    add first test

--- SzkolaTestowJednostkowych_UnitTests/Basics/CalculatorTests.cs
+++ SzkolaTestowJednostkowych_UnitTests/Basics/CalculatorTests.cs
@@ -1,17 +1,26 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SzkolaTestowJednostkowych.Basics;
 
 namespace SzkolaTestowJednostkowych_UnitTests.Basics
 {
-    class CalculatorTests
+    public class CalculatorTests
     {
         //NazwaMetody_Scenariusz_OczekiwanyRezultat
         public void Add_WhenCalled_ShouldReturnSum()
         {
+            //Arrange
+            var calculator = new Calculator();
 
+            //Act
+            var result = calculator.Add(1, 2);
+
+            //Assert
+            Assert.Equals(3, result);
         }
     }
 }
